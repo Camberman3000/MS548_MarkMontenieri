9c2485fad3a1bbfd0ba829b5c6edf5e39aaec0fd
Mark M <markmontenieri@gmail.com>
2024-03-15 14:21:49 -0400

    Ready to turn in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Mark Montenieri - MS548
 # Week 1 project - Estimated time to complete - 4 hours
+# Actual time to complete - 7 hours
 # from textblob lib import TextBlob method
 from textblob import TextBlob
 
@@ -14,10 +15,10 @@ def task_multiply():
     main()
 
 
-def validate_num(num, num2):#  validate input
+def validate_num(num, num2):  # validate input
     while True:
         if num.isdigit() and num2.isdigit():
-            return
+            return num, num2
         else:
             print("One or more of your inputs was not a number, please start over.")
             task_multiply()
@@ -25,21 +26,33 @@ def validate_num(num, num2):#  validate input
 
 
 def task_divide():
-    # perform multiplication
-    variable = input("Type a number: ")
-    print("Your number squared is " + str(int(variable) ** 2))
+    # perform division
+    first_num = input("Type first number: ")
+    second_num = input("Type second number: ")
+    validate_num(first_num, second_num)
+    product = int(first_num) / int(second_num)
+    print((str(first_num) + " divided by " + (str(second_num) + " = " + (str(product)))))
+    main()
 
 
 def task_add():
-    # perform multiplication
-    variable = input("Type a number: ")
-    print("Your number squared is " + str(int(variable) ** 2))
+    # perform addition
+    first_num = input("Type first number: ")
+    second_num = input("Type second number: ")
+    validate_num(first_num, second_num)
+    product = int(first_num) + int(second_num)
+    print((str(first_num) + " plus " + (str(second_num) + " = " + (str(product)))))
+    main()
 
 
-def task_subtract():
-    # perform multiplication
-    variable = input("Type a number: ")
-    print("Your number squared is " + str(int(variable) ** 2))
+def task_textblob_sentiment():
+    # perform sentiment analysis using textblob
+    text = input("Type a sentence to analyze, which will return the sentiment (Higher polarity = more positive & "
+                 "Higher subjectivity = more opinionated): ")
+    blob = TextBlob(text)
+    sentiment = blob.sentiment
+    print("Sentiment analysis results: " + str(sentiment))
+    main()
 
 
 def main():
@@ -49,7 +62,7 @@ def main():
         print("1. Multiply two numbers")
         print("2. Divide two numbers")
         print("3. Add two numbers")
-        print("4. Subtract two numbers")
+        print("4. Sentiment analysis")
         print("5. Exit")
 
         choice = input("Enter your choice (1-5): ")
@@ -62,10 +75,10 @@ def main():
             elif choice == 3:
                 task_add()
             elif choice == 4:
-                task_subtract()
+                task_textblob_sentiment()
             elif choice == 5:
                 print("Exiting program")
-                break
+                quit()
         else:
             print("Invalid choice. Please choose a number between 1 and 5")
 
